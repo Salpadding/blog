0f1b22ebd260fd53ecbd1d4abe81e78f96189d87
zhuyingjie <m6567fc@outlook.com>
2020-07-21 13:42:31 +0800

    add deploy.js

---
+++ deploy.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const COS = require('cos-nodejs-sdk-v5')
+const config = require('./deploy-config.json')
+const fs = require('fs')
+const path = require('path')
+
+const cos = new COS({
+    SecretId: config.SecretId,
+    SecretKey: config.SecretKey
+});
+
+function getFilesSync(ret, filePath) {
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+        const files = fs.readdirSync(filePath)
+        files.forEach(f => getFilesSync(ret, path.join(filePath, f)))
+    } else {
+        ret.push(filePath)
+    }
+}
+
+function main() {
+    if (!config.Dist || !fs.statSync(config.Dist).isDirectory) {
+        console.error(config.Dist + " is not a valid directory")
+        return
+    }
+    const files = []
+    const tasks = []
+    getFilesSync(files, config.Dist)
+    files.forEach(f => {
+        const key = path.relative(config.Dist, f)
+
+        const p = new Promise((resolve, reject) => {
+            cos.putObject({
+                Bucket: config.Bucket,
+                Region: config.Region,
+                Key: key,
+                Body: fs.createReadStream(f)
+            }, (err, data) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                console.log(`deploy ${f} success`)
+                resolve(f)
+            })
+        })
+
+        tasks.push(p)
+    })
+
+    Promise
+    .all(tasks)
+    .then(() => console.log('\n'))
+    .catch(console.error)
+}
+
+
+main()
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+hexo generate
+node ./deploy.js
